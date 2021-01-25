d68f5c03a5bb7cc4cfeb4562ee7515c8fe0f831a
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2021-01-25 13:27:45 -0300

    new file: TeamController

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace voetbal_api.Controllers
+{
+    [ApiController]
+    [Route("team")]
+    public class TeamController : ControllerBase
+    {
+        
+    }
+}
