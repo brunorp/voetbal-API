41b5ff9a5ecf219502d65f3a2585300ce41e99fb
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2021-01-25 13:34:16 -0300

    feat: post team

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using voetbal_api.Data;
+using voetbal_api.Models;
 
 namespace voetbal_api.Controllers
 {
@@ -6,6 +9,14 @@ namespace voetbal_api.Controllers
     [Route("team")]
     public class TeamController : ControllerBase
     {
-        
+        [HttpPost]
+        [Route("")]
+        public async Task<Team> postTeam([FromServices] DataContext context, [FromBody] Team team)
+        {
+            context.Teams.Add(team);
+            await context.SaveChangesAsync();
+
+            return team;
+        }
     }
 }
