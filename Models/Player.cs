fcfaf955b9e070717ef2081b0d48493073c00ad0
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2021-01-25 11:34:10 -0300

    new file: Player model

---
+++ Models/Player.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace voetbal_api.Models
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; private set; }
+
+        [Required(ErrorMessage="Field \"Name\" is required.", AllowEmptyStrings=false)]
+        [MaxLength(50, ErrorMessage="Name max length is 50 characters")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage="Field \"Position\" is required.", AllowEmptyStrings=false)]
+        [MaxLength(30, ErrorMessage="Position max length is 50 characters")]
+        public string Position { get; set; }
+    }
+}
